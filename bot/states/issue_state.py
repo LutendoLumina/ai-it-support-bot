b3eccc204508f269592d857c38370014e11f9ce6
Lutendo Matshidze <Lupreshie@gmail.com>
2026-01-02 23:56:27 +0200

    Updated code review file for reviewdog fixations

--- bot/states/issue_state.py
+++ bot/states/issue_state.py
@@ -1,6 +1,6 @@
 def issue_details_state():
     print("\nThanks for that.")
-    printt("\nPlease describe the issue in a bit more detail:")
+    print("\nPlease describe the issue in a bit more detail:")
 
     user_input = input(">").strip()
 
